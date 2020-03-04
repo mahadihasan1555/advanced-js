ae06b8bef11bf4c53b14fecc760a32ae920abb07
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-03-04 13:30:51 +0600

    null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+//let pakhi;
+
+
+//function addd(num1, num2){
+    //console.log(num1, num2);
+    
+}
+//const result = add(13);
+//console.log(result);
+
+
+//const premik = {name: "smart", phone: 23434};
+//console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
