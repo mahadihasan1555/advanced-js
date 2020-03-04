d2a67be9de0e720f9af54368c7a5f4993ea9fb4e
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-03-04 12:57:19 +0600

    truthy-falsy value

---
+++ true-false.js
@@ -0,0 +1,25 @@
+//const name = "0";
+
+
+// Falsy:
+// false
+//0
+//""
+// undefined
+//null
+//NaN
+//truthy:
+// '0', ' ', []
+
+let name = null;
+console.log(name);
+
+
+if (name ){
+    console.log("condition is true");
+    
+}
+else{
+    console.log("condition is false");
+    
+}
